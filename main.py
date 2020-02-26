e48e695adf628a9be8076758cbcc33fea59c2150
bting <bting@lscm.hk>
2020-02-26 19:36:51 +0800

    added reward (longer bar)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import math
+import random
 import pygame
 
 class Wall():
@@ -32,6 +33,35 @@ class Brick():
                         [self.x         , self.y + self.h],
                         [self.x + self.w, self.y + self.h]]
         return
+    
+    def get_cord_int(self):
+        return (int(self.cx), int(self.cy))
+
+class Reward():
+    def __init__(self, x, y, r=7, vx=0, vy=1):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.vx = vx
+        self.vy = vy
+        self.update()
+        self.speed = 300
+
+    def update(self):
+        self.x1 = self.x - self.r
+        self.x2 = self.x + self.r
+        self.y1 = self.y - self.r
+        self.y2 = self.y + self.r
+        return
+
+    def nex(self, dt):
+        self.x = self.x + self.vx * dt * self.speed
+        self.y = self.y + self.vy * dt * self.speed
+        self.update()
+        return
+    
+    def get_cord_int(self):
+        return (int(self.x), int(self.y))
 
 class Seed():
     def __init__(self, x, y, r=10, vx=1, vy=-1):
@@ -84,12 +114,21 @@ class Stand():
         self.y2 = self.y + self.h
         return
     
-    def move_left(self, dt):
+    def move_left(self, dt, bound):
         self.x = self.x - dt * 500
         self.update()
+        if self.x < bound:
+            self.x = bound
 
-    def move_right(self, dt):
+    def move_right(self, dt, bound):
         self.x = self.x + dt * 500
+        self.update()
+        if self.x + self.w > bound:
+            self.x = bound - self.w
+    
+    def enlong(self):
+        self.x -= 10
+        self.w += 20
         self.update()
 
 class Game():
@@ -107,6 +146,7 @@ class Game():
         self.PINK = (255, 120, 255)
         self.WHITE = (255, 255, 255)
         self.BLACK = (0, 0, 0)
+        self.YELLOW = (255, 255, 102)
 
         ## OBJECTS
         self.wall = Wall(0, 0, self.win_w, self.win_h)
@@ -128,12 +168,15 @@ class Game():
         self.stands = []
         stand = Stand(self.win_w/2, self.win_h-20-10, 150, 20)
         self.stands.append(stand)
+    
+        self.rewards = []
+
 
         ## INIT
         pygame.init()
         pygame.init()
         self.window_surface = pygame.display.set_mode((self.wall.x_max, self.wall.y_max))
-        pygame.display.set_caption('Brick Breaker @@"',)
+        pygame.display.set_caption('Brick Breaker @@ by benting180"',)
         self.head_font = pygame.font.SysFont(None, 30)
         text_surface = self.head_font.render('Hello World!', True, (255,255,255))
         self.window_surface.blit(text_surface, (10, 10))
@@ -154,13 +197,22 @@ class Game():
 
         for s in self.stands:
             pygame.draw.rect(self.window_surface, self.BLUE, (s.x, s.y, s.w, s.h), 1)
+        
+        for r in self.rewards:
+            pygame.draw.circle(self.window_surface, self.YELLOW, r.get_cord_int(), r.r, 1)
 
         self.update_scoring()
         
     def update(self, dt):
         for s in self.seeds:
             s.nex(dt)
+
+        for r in self.rewards:
+            r.nex(dt)
+
         return
+    
+
 
     def collide_wall(self):
         # w/ wall
@@ -174,11 +226,14 @@ class Game():
                 print("remove!")
                 self.seeds.pop(i)
             if b.x + b.r > self.wall.x_max:
+                b.x = self.wall.x_max - b.r
                 b.flip_vx()
             if b.x - b.r < self.wall.x_min:
+                b.x = self.wall.x_min + b.r
                 b.flip_vx()
             if b.y - b.r < self.wall.y_min:
                 b.flip_vy()
+                b.y = self.wall.y_min + b.r
         # copy_seeds = self.seeds.copy()
         # for i in remove_i:
         #     print(i)
@@ -241,14 +296,23 @@ class Game():
                         s.flip_vy()
             
             if hit:
+                ## create rewards
+                self.create_reward(*(self.bricks[i].get_cord_int()))
                 self.bricks.pop(i)
                 self.score_brick()
         return
+    
+    def create_reward(self, x, y):
+        if random.random() > 0.1:
+            reward = Reward(x, y)
+            self.rewards.append(reward)
+
     def collide_stand(self):
         for st in self.stands:
             for s in self.seeds:
                 if s.y2 > st.y1:
                     if s.x2 > st.x1 and s.x1 < st.x2:
+                        s.y = st.x1 - s.r
                         s.flip_vy()
         return
 
@@ -259,20 +323,33 @@ class Game():
         if len(self.bricks) == 0:
             sys.exit()
                     
-    def check_collision(self):
+    def check_seed_collision(self):
         self.collide_wall()
         self.collide_brick()
         self.collide_stand()
         return
+    
+    def check_reward_collision(self):
+        #  for st in self.stands:
+        #     for s in self.seeds:
+        #         if s.y2 > st.y1:
+        #             if s.x2 > st.x1 and s.x1 < st.x2:
+        #                 s.flip_vy()
+        st = self.stands[0]
+        for j, r in enumerate(self.rewards):
+            if r.y2 > st.y1:
+                if r.x2 > st.x1 and st.x1 < st.x2:
+                    self.rewards.pop(j)
+                    st.enlong()
 
     def stand_left(self, dt):
         for st in self.stands:
-            st.move_left(dt)
+            st.move_left(dt, 10)
         return
     
     def stand_right(self, dt):
         for st in self.stands:
-            st.move_right(dt)
+            st.move_right(dt, self.win_w-10)
         return
     
     def add_seed(self):
@@ -384,7 +461,8 @@ while True:
         pygame.display.update()
         dt = time.time() - t1
         game.update(dt)
-        game.check_collision()
+        game.check_seed_collision()
+        game.check_reward_collision()
         game.check_endgame()
 
         pygame.event.pump() # process event queue
